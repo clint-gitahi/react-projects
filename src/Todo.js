548ffdd94091595b49c1d8137287480e131a941d
clintgitahi <clinton@yumkenya.com>
2019-06-12 12:32:46 +0300

    editing

--- src/Todo.js
+++ src/Todo.js
@@ -4,28 +4,58 @@ class Todo extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { isEditing: false };
+    this.state = { isEditing: false, task: this.props.task };
 
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
+
+  toggleForm() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    });
+  }
+
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
+
+  handleUpdate(evt) {
+    evt.preventDefault();
+
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+
   render() {
     let result;
 
     if (this.state.isEditing) {
       result = (
         <div>
-          <form>
-            <input type="text" />
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
           </form>
         </div>
       );
     } else {
       result = (
         <div>
-          <button>Edit</button>
+          <button onClick={this.toggleForm}>Edit</button>
           <button onClick={this.handleRemove}>X</button>
 
           <li>{this.props.task}</li>
--- src/TodoList.js
+++ src/TodoList.js
@@ -12,6 +12,7 @@ class TodoList extends Component {
 
     this.create = this.create.bind(this);
     this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
   }
 
   create(newTodo) {
@@ -26,6 +27,17 @@ class TodoList extends Component {
     });
   }
 
+  update(id, updatedTask) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, task: updatedTask };
+      }
+      return todo;
+    });
+
+    this.setState({ todos: updatedTodos });
+  }
+
   render() {
     const todos = this.state.todos.map(todo => {
       return (
@@ -34,6 +46,7 @@ class TodoList extends Component {
           id={todo.id}
           task={todo.task}
           removeTodo={this.remove}
+          updateTodo={this.update}
         />
       );
     });
