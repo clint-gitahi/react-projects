8f881dd823d4635cdc16472678df768a955b49e8
clintgitahi <clinton@yumkenya.com>
2019-06-12 13:20:32 +0300

    adding styles to todo

--- src/Todo.js
+++ src/Todo.js
@@ -46,7 +46,7 @@ class Todo extends Component {
 
     if (this.state.isEditing) {
       result = (
-        <div>
+        <div className="Todo">
           <form onSubmit={this.handleUpdate}>
             <input
               type="text"
@@ -60,16 +60,23 @@ class Todo extends Component {
       );
     } else {
       result = (
-        <div>
-          <button onClick={this.toggleForm}>Edit</button>
-          <button onClick={this.handleRemove}>X</button>
-
+        <div className="Todo">
           <li
-            className={this.props.completed ? "completed" : ""}
+            className={
+              this.props.completed ? "Todo-task completed" : "Todo-task"
+            }
             onClick={this.handleToggle}
           >
             {this.props.task}
           </li>
+          <div className="Todos-buttons">
+            <button onClick={this.toggleForm}>
+              <i className="fa fa-pen" />
+            </button>
+            <button onClick={this.handleRemove}>
+              <i className="fas fa-trash" />
+            </button>
+          </div>
         </div>
       );
     }
